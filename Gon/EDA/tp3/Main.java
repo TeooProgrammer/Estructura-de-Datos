0c44c583194fd6d53fb722198a652bcf1ddacecc
TeooProgrammer <mateojavieroscar@gmail.com>
2025-10-11 16:57:29 -0300

    AA

--- Gon/EDA/tp3/Main.java
+++
@@ -1,9 +0,0 @@
-package tp3;
-
-
-public class Main {
-    public static void main(String[] args) {
-        ArchivoDeEnteros enteros = new ArchivoDeEnteros("enteros.dat");
-        enteros.mostrarElementos();
-    }
-}
